c084a4db042abb1d6e2f4f000cb5caf87f8eb956
Federico <fede.senmartin@gmail.com>
2025-03-03 21:52:22 -0300

    remove credentials include

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -4,7 +4,6 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:1337/graphql',
-    credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
